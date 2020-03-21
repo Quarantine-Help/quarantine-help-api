a7147559390e0275703c3ed31cd97b81d6e537ed
Tony Thomas <tony.thomas@keyflow.se>
2020-03-21 14:07:24 +0100

    Add pip install to deploy steps

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -2,6 +2,7 @@ source ~/.pyenv/versions/quarantine_proj_env/bin/activate
 cd /root/quarantined_backend/
 git stash
 git pull origin master
+pip install -r requirements.txt
 # remove some pyc files
 sudo systemctl stop nginx
 sudo systemctl stop gunicorn.socket
