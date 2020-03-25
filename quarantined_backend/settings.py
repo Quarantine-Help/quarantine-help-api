0ee5c484910a2ee09b2829abd9aeae0c9ca8dcc8
Tony Thomas <tony.thomas@keyflow.se>
2020-03-25 19:22:02 +0100

    CORS_ALLOW_CREDENTIALS to True

--- quarantined_backend/settings.py
+++ quarantined_backend/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 
 # Needs to be fixed later
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
