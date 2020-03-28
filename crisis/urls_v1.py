de43dbee55b131d76626fee5ed6c185ba9891b63
Tony Thomas <tony.thomas@keyflow.se>
2020-03-28 14:04:26 +0100

    Remove create API on crisis/participants/

--- crisis/urls_v1.py
+++ crisis/urls_v1.py
@@ -2,15 +2,9 @@ from django.conf.urls import url
 from django.urls import path
 
 from crisis.views import CrisisListAPIV1, ListAffectedParticipantsAPIV1
-from management.views import CreateParticipantsAPIV1
 
 urlpatterns = [
     url(r"^$", CrisisListAPIV1.as_view(), name="list_crisies"),
-    path(
-        "<int:crisis_id>/participants/",
-        CreateParticipantsAPIV1.as_view(),
-        name="create_list_participants",
-    ),
     path(
         "<int:crisis_id>/affected-participants/",
         ListAffectedParticipantsAPIV1.as_view(),
