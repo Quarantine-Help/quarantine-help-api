e0a38c8dcaf8a0788cca31ea9ff4716114c35710
Tony Thomas <01tonythomas@gmail.com>
2020-04-13 17:30:11 +0200

    Travis to run tests ?

---
+++ quarantined_backend/local_settings_travis.py
@@ -0,0 +1,26 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+import os
+
+from quarantined_backend.settings import BASE_DIR
+
+SECRET_KEY = "%5r@$^)+$$#$%ˆˆ%ˆˆˆˆ%$%%$&ˆˆFFDFDSFSDF@$@#DSSFDZzzzSAe3n%fm_"
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# We do not support sqlite anymore
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "travis_ci_test",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "",
+    }
+}
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+ALLOWED_HOSTS = []
